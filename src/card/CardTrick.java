3057ca5ddd960cea9d9a66a48f667663ac72141e
ashvi <ashvi@10.0.0.89>
2024-02-04 21:26:06 -0500

    initial

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,32 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package card;
 
-/**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * Modified by: Ashvir Aulakh
+ * Student Number: 991716285
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
+        
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+
+  
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(generateRandomValue()); 
+            c.setSuit(Card.SUITS[generateRandomNumber(0, 3)]); 
+            magicHand[i] = c; 
         }
+
+  
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter card value (1-13): ");
+        int userValue = scanner.nextInt();
+
+        System.out.print("Enter card suit (0-3): ");
+        int userSuit = scanner.nextInt();
+
+        Card userCard = new Card();
+        userCard.setValue(userValue);
+        userCard.setSuit(Card.SUITS[userSuit]);
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.equals(userCard)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
     }
-    
+
+    private static int generateRandomNumber(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt(max - min) + min;
+    }
+
+    private static int generateRandomValue() {
+        return generateRandomNumber(1, 14);
+    }
 }
+
+
